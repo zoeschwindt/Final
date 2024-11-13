5b03dc458b225af7ed9e9d84e3b82c70e65bc5c7
zoeschwindt <schwindtzoe380@gmail.com>
2024-11-13 15:38:22 -0300

    Create menuu

--- Assets/Nave.cs
+++ Assets/Nave.cs
@@ -2,17 +2,38 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+[RequireComponent(typeof(Rigidbody2D))]
+
 public class Nave : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    [SerializeField] float movSpeed;
+    private float playerMov;
 
-    // Update is called once per frame
+    private int fuerzaDeSalto = 5;
+    private bool estaSaltando = false;
+
     void Update()
     {
         
+    }
+    void Movimiento() 
+    {
+        playerMov = Input.GetAxisRaw("Horizontal");
+        transform.position += new Vector3(playerMov * movSpeed * Time.deltaTime, 0f, 0f);
+
+        if (Input.GetKeyDown(KeyCode.Space) && estaSaltando == false)
+        {
+            GetComponent<Rigidbody2D>().velocity = new Vector2(0, fuerzaDeSalto);
+            estaSaltando = true;
+        }
+
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "piso")
+        {
+            estaSaltando = false;
+        }
     }
 }
